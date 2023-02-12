f7c64445f8ee7446076656791c8e017d3337b29b
Aditya Suryadi <adityasuryaditya@gmail.com>
2023-02-12 15:15:13 +0700

    feat:add bash build image

---
+++ build_push_image_karsajbos.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# build image dari dockerfile dengan nama item-app dan tag v1
+
+echo "build docker"
+docker build -t karsajobs:latest . 
+
+echo "============================================================"
+
+# lihat semua image yang ada di local
+echo "list docker image"
+docker images
+
+echo "============================================================"
+# mengubah nama image yang sudah di buat ke nama image sesuai dengan ketentuan nama yang akan di push ke repository 
+echo "rename docker image"
+docker tag karsajobs:latest ghcr.io/adityasuryadi/karsajobs:latest
+echo "============================================================"
+# login ke githu container menggunakan personal access token yang sudah di set sebelum nya
+echo "login github"
+
+echo %TOKEN_GITHUB%| docker login ghcr.io -u adityasuryadi --password-stdin
+echo "============================================================"
+# push image yang sudah di rename ke github container
+echo "push ke github container registry"
+docker push ghcr.io/adityasuryadi/karsajobs:latest
+echo "============================================================"
